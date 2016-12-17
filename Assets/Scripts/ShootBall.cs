ac2029f71e51347feebfbaa2cadeba0c8858fa66
Lanner Daniel - S1410238045 <S1410238045@hagenberg.fhooe.at>
2016-12-17 12:11:08 +0100

    ball shooting script

---
+++ Assets/Scripts/ShootBall.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootBall : MonoBehaviour {
+
+
+    public Rigidbody ball;
+    public float thrust;
+    private Camera m_Camera;
+
+    bool shooted = false;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (!shooted) {
+            var forward = transform.forward * 3;
+            var position = forward + transform.position;
+            ball.transform.position = position;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            shooted = true;
+            var transformVector = transform.forward * thrust;
+            transformVector.y += 5;
+            ball.AddForce(transformVector, ForceMode.Impulse);
+            ball.useGravity = true;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+}
