985da1c4dc58129be1b0aa13c94d4263f04144d2
Andreas Sturmberger <S1410238029@hagenberg.fhooe.at>
2016-12-18 11:19:44 +0100

    [TASK] A New Beginning

---
+++ Assets/Scripts/EntitySpawner.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class EntitySpawner : NetworkBehaviour {
+
+    public GameObject bottlePrefab;
+    public GameObject ballPrefab;
+
+    public override void OnStartServer()
+
+    {
+        Vector3 spawnPosition = new Vector3(0.0f, 1.0f, 1.0f);
+        Quaternion spawnRotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+
+        GameObject bottle = (GameObject)Instantiate(bottlePrefab, spawnPosition, spawnRotation);
+        NetworkServer.Spawn(bottle);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+    public Camera cam; // Drag camera into here
+
+    void Start()
+    {
+        // IF I'M THE PLAYER, STOP HERE (DON'T TURN MY OWN CAMERA OFF)
+        if (isLocalPlayer) return;
+
+        // DISABLE CAMERA AND CONTROLS HERE (BECAUSE THEY ARE NOT ME)
+        cam.enabled = false;
+        //GetComponent<PlayerControls>().enabled = false;
+        //GetComponent<PlayerMovement>().enabled = false;
+    }
+
+
+    void Update()
+    {
+        if(!isLocalPlayer)
+        {
+            return;
+        }
+
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+    }
+}
