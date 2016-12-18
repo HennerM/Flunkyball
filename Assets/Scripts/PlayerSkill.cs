4c02d09c0902b2da9b259cd932aa4caa684f1f95
Pauzenberger Elena - S1410238055 <S1410238055@hagenberg.fhooe.at>
2016-12-18 13:50:57 +0100

    Added PlayerSkills and functionallity for drinking

---
+++ Assets/Scripts/PlayerSkill.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine.Networking;
+
+namespace Assets.Scripts
+{
+    public class PlayerSkill : NetworkBehaviour
+    {
+        private int drinkingCapacity = 100;
+        private int drinkingSpeed = 100;
+        private bool isThrowingUp = false;
+        private bool isSleeping = false;
+        private int runningSpeed = 100;
+        private int throwingStrength = 10;
+
+
+        public int DrinkingCapacty { get; set; }
+        public int DrinkingSpeed { get; set; }
+        public int RunningSpeed { get; set; }
+        public bool IsThrowingUp { get; set; }
+        public bool IsSleeping { get; set; }
+        public int ThrowingStrength { get; set; }
+
+    }
+}
